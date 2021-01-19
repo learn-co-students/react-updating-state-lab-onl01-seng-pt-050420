8044e88296ce60b68fc568c3c9bf1580686cb371
Ghameerah McCullers <mccullers.ghameerah@gmail.com>
2021-01-19 16:12:45 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import YouTubeDebugger from './components/YouTubeDebugger';
 ReactDOM.render(
   <div>
     <DigitalClicker />
-    <YouTubeDebugger />
-  </div>,
+    {/*comments in JSX*/}
+
+    {/*<YouTubeDebugger />*/}
+</div>,
   document.getElementById('global')
 );
